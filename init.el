c266a3dc6adf2504fc7d3005cd05a13d841de862
YouLanjie <ylj18926385312@163.com>
2023-03-11 19:36:39 +0800

    提高加载速度

--- init.el
+++ init.el
@@ -6,39 +6,43 @@
 
 ;;; Code:
 
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-; 设定源码加载路径
-
+;; 设置默认的custom设置保存文件（私人）
 (setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
 (load custom-file 'no-error 'no-message)
-;; 设置默认的custom设置保存文件（私人）
-(require 'init-custom)
-;; 读取分发出去的custom设置
 
 ;; 加载模块
 
-;; 设置类
-;; 插件
-(require 'init-packages)
-;; UI
-(require 'init-ui)
-;; 默认设置
-(require 'init-better-defaults)
-;; 按键
-(require 'init-keybindings)
-;; Org-mode
-(require 'init-org)
-;; 自动插入
-(require 'init-auto-insert)
-;; 快速输入
-(require 'init-quickly-input-c)
+;; 设定源码加载路径
+(add-to-list 'load-path "~/.emacs.d/lisp/")
 
-;; 自己编写的“插件”
-;; 自动保存
-(require 'init-auto-save)
-;; 打开文件
-(require 'init-open-files)
-;; 切换透明度
-(require 'init-loop-alpha)
+(let ((gc-cons-threshold most-positive-fixnum)
+      (file-name-handler-alist nil))
+  ;; 读取分离出去的custom设置
+  (require 'init-custom)
+
+  ;; 设置类
+  ;; 插件
+  (require 'init-packages)
+  ;; UI
+  (require 'init-ui)
+  ;; 默认设置
+  (require 'init-better-defaults)
+  ;; 按键
+  (require 'init-keybindings)
+  ;; Org-mode
+  (require 'init-org)
+  ;; 自动插入
+  (require 'init-auto-insert)
+  ;; 快速输入
+  (require 'init-quickly-input-c)
+
+  ;; 自己编写的“插件”
+  ;; 自动保存
+  (require 'init-auto-save)
+  ;; 打开文件
+  (require 'init-open-files)
+  ;; 切换透明度
+  (require 'init-loop-alpha)
+  )
 
 ;;; init.el ends here
--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -22,41 +22,35 @@
 ;; 插件管理
 (eval-when-compile
   (require 'use-package))
-; 主题
+;; 主题
 (use-package atom-one-dark-theme)
-; 开启evil模式 -- 使用vim键位
+;; 开启evil模式 -- 使用vim键位
 (use-package evil
   :ensure t
   :init (evil-mode))
+;; 启动速度检查
 (use-package benchmark-init
   :init (benchmark-init/activate)
   :hook (after-init . benchmark-init/deactivate))
-; 更好的状态栏
+;; 更好的状态栏
 (use-package smart-mode-line
   :ensure t
   :init
   (setq sml/no-confirm-load-theme t)
   (setq sml/theme 'respectful)
   (sml/setup))
-; 像vim一样平滑的滚动
-;; (use-package smooth-scrolling
-;;   :init (smooth-scrolling-mode)
-;;   :config
-;;   (setq scroll-margin 3
-;; 	scroll-conservatively 9999
-;; 	scroll-step 1))
-; 使用像vim一样的Powerline（状态栏）
+;; 使用像vim一样的Powerline（状态栏）
 (use-package powerline-evil
   :init
   (powerline-evil-vim-color-theme)
   :config
   (require 'powerline)
   (display-time-mode t))
-; 显示可用键位
+;; 显示可用键位
 (use-package which-key
   :ensure t
   :init (which-key-mode))
-; 自动补全
+;; 自动补全
 (use-package company
   :ensure t
   :init (global-company-mode)
@@ -67,7 +61,7 @@
   (setq company-show-numbers t) ;; 给选项编号 (按快捷键 M-1、M-2 等等来进行选择).
   (setq company-selection-wrap-around t)
   (setq company-transformers '(company-sort-by-occurrence))) ; 根据选择的频率进行排序，读者如果不喜欢可以去掉
-; 补全模板
+;; 补全模板
 (use-package yasnippet
   :ensure t
   :init
@@ -90,19 +84,19 @@
   (add-to-list 'yas-snippet-dirs (locate-user-emacs-file "~/.emacs.d/snippets/"))
   :bind
   (:map yas-minor-mode-map ("S-<tab>" . yas-expand)))
-; 默认补全模板
+;; 默认补全模板
 (use-package yasnippet-snippets
   :ensure t
   :after yasnippet)
-; 针对org-mode符号的美化（因字体问题取消）
+;; 针对org-mode符号的美化（因字体问题取消）
 (use-package org-bullets
   ; :custom
   ; (org-bullets-bullet-list '("◉" "○""✸" "✿" "✜" "◆""▶"))
   ; (org-ellipsis "⤵")
   :hook (org-mode . org-bullets-mode))
 ;; 命令输入
-(use-package smex)
-; 语法检查，比vim的好
+;; (use-package smex)
+;; 语法检查，比vim的好
 (use-package flycheck
   :ensure t
   :config
@@ -116,7 +110,7 @@
   (setq flycheck-clang-analyzer-executable "clang")
   :hook
   (c-mode . flycheck-clang-analyzer-setup))
-; LSP语法解析
+;; LSP语法解析
 (use-package lsp-mode
   :ensure t
   :init
@@ -129,7 +123,7 @@
   :config
     (setq lsp-completion-provider :none) ;; 阻止 lsp 重新设置 company-backend 而覆盖我们 yasnippet 的设置
     (setq lsp-headerline-breadcrumb-enable t))
-; git管理工具
+;; git管理工具
 (use-package magit
   :ensure t)
 (use-package window-numbering
@@ -138,13 +132,22 @@
   :ensure t
   :init (require 'valign)
   :hook (org-mode-hook . valign-mode))
+
+;; 像vim一样平滑的滚动
+;; (use-package smooth-scrolling
+;;   :init (smooth-scrolling-mode)
+;;   :config
+;;   (setq scroll-margin 3
+;; 	scroll-conservatively 9999
+;; 	scroll-step 1))
+
 ;; Org图表绘图
-;(use-package gnuplot)
-; Undo tree解决emacs的撤回痛点（卡顿大）
-;(use-package undo-tree
-;  :ensure t
-;  :init (global-undo-tree-mode))
-;  :config  (setq undo-tree-history-directory-alist `(("." . "~/.emacs.d/.cache/undo/")))
+;; (use-package gnuplot)
+;;  Undo tree解决emacs的撤回痛点（卡顿大）
+;; (use-package undo-tree
+;;   :ensure t
+;;   :init (global-undo-tree-mode))
+;;   :config  (setq undo-tree-history-directory-alist `(("." . "~/.emacs.d/.cache/undo/")))
 
 (provide 'init-packages)
 
